b31820fded54f7692d1cfd1da517d93352e76fec
PC Casa <felipe.devel@gmail.com>
2023-08-15 00:48:20 -0300

    continuação - verificar se o usuário tem permissão

--- app/src/Controller/ApiController.php
+++ app/src/Controller/ApiController.php
@@ -8,10 +8,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Model\Table\UsuariosTable;
 use Cake\Core\Configure;
 use Cake\Http\Exception\ForbiddenException;
 use Cake\Http\Exception\NotFoundException;
 use Cake\Http\Response;
+use Cake\ORM\TableRegistry;
 use Cake\View\Exception\MissingTemplateException;
 use Exception;
 
@@ -35,8 +37,10 @@ class ApiController extends AppController
             //VALIDAR FORMULÁRIO E USUÁRIO
             $this->validarFormulario();
             //VALIDAR SE TEM PERMISSÃO (PERFIL USUARIO)
-            //VALIDAR SE TEM SALDO
-            //VERIFICAR SE ESTÁ AUTORIZADO A REALIZAR A TRANSFERENCIA
+            $this->validarPermissao();
+            //VERIFICAR SALDO
+            $saldo = $this->verificarSaldo();
+            //VERIFICAR SE TEM ESTA AUTORIZADO A TRANSFERIR (EXTERNO)            
             //NOTIFICAR O RECEBEDOR
             //ATUALIZAR SALDO DO RECEBEDOR E DO PAGADOR
 
@@ -50,6 +54,35 @@ class ApiController extends AppController
         return $this->responseJson($info);
     }
 
+    private function verificarSaldo()
+    {
+
+        $pagador_id = $this->request->getData('payer') ?? null;
+        $valor_transferencia = $this->request->getData('value') ?? null;
+
+
+        $tabCarteira = TableRegistry::getTableLocator()->get('Carteiras');
+        $saldo = $tabCarteira->getSaldo($pagador_id);
+
+        if ($saldo < $valor_transferencia) {
+            throw new Exception('Você não tem saldo suficiente para essa transferência');
+        }
+
+        return $saldo;
+    }
+
+
+    private function validarPermissao()
+    {
+        $check_pagador = $this->request->getData('payer') ?? null;
+        $tabUsuarios = TableRegistry::getTableLocator()->get('Usuarios');
+
+        $objUsuarioPagador = $tabUsuarios->getUsuario($check_pagador);
+
+        if ($objUsuarioPagador->tipo->key == UsuariosTable::TIPO_LOJA) {
+            throw new Exception('Lojistas não possuem permissão para realizar transferência, desculpe - Erro 5601');
+        };
+    }
 
     private function validarFormulario()
     {
@@ -61,14 +94,30 @@ class ApiController extends AppController
         //VERIFICAR SE TODOS OS CAMPOS OBRIGATÓRIOS FORAM PREENCHIDOS
         if (empty($check_pagador) || empty($check_recebedor) || empty($check_valor)) {
 
-            die('errou');
-            throw new Exception('Verifique se todos os campos obrigatórios do payload foram preenchidos');
+            throw new Exception('Verifique se todos os campos obrigatórios do payload foram preenchidos - Erro 5501');
         }
 
 
-        //VERIFICAR SE PAGADO E RECEBEDOR EXISTEM NA BASE
-        
-        
+        //VERIFICAR SE PAGADOR E RECEBEDOR EXISTEM NA BASE
+        $tabUsuarios = TableRegistry::getTableLocator()->get('Usuarios');
+        $usuarios_validos = $tabUsuarios->verificarUsuarioRecebedorEPagador($check_pagador, $check_recebedor);
+
+        if (empty($usuarios_validos)) {
+            throw new Exception('Verifique se você preencheu corretamente as identificações dos campos payer ou payee - Erro 5502');
+        }
+
+
+        //NÃO PERMITIR FAZER TRANSFERENCIA PARA A MESMA CONTA
+        if ($check_pagador === $check_recebedor) {
+            throw new Exception('Não é possível fazer transferência para a mesma conta - Erro 5503');
+        }
+
+
+        $check_valor = floatval($check_valor);
+
         //VERIFICAR SE VALOR DE TRANSFERÊNCIA É POSITIVO
+        if ($check_valor <= 0) {
+            throw new Exception('Informe um valor válido de transferencia - Erro 5504');
+        }
     }
 }
